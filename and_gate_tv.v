15ce655a04786b9b130e53cf2803d364c056fec2
Devendra Singh <ds4716450@gmail.com>
2026-06-22 11:11:31 +0530

    Initial Verilog codes

---
+++ and_gate_tv.v
@@ -0,0 +1,17 @@
+module tb;
+reg a,b;
+wire y;
+
+and_gate dut(a,b,y);
+
+initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0,tb);
+
+    a=0; b=0;
+    #10 a=0; b=1;
+    #10 a=1; b=0;
+    #10 a=1; b=1;
+    #10 $finish;
+end
+endmodule
